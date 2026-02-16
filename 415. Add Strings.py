2c7b1999baf1de7b5de710c28db67365c024f273
Disha Pure <143890086+dishapure@users.noreply.github.com>
2026-02-16 15:18:45 +0530

    Create 415. Add Strings.py

---
+++ 415. Add Strings.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def addStrings(self, num1, num2):
+        num1=int(num1)
+        num2=int(num2)
+        result=num1+num2
+        result=str(result)
+        return result
+        
